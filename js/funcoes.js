d8c41f41b96974328958ca2e5e54ea50b3ee5931
marcoos127 <mlkzika60@live.com>
2018-08-23 21:49:33 -0300

    v2

--- js/funcoes.js
+++ js/funcoes.js
@@ -4,3 +4,12 @@ function trocaFonte() {
 function Fontetroca() {
 	document.getElementById("demo").style.fontSize="18px"
 }
+function Ocultarelemento() {
+	document.getElementById("demo").style.display="none"
+}
+function naoocultar() {
+	document.getElementById("demo").style.display="block"
+}
+function calculo() {
+	document.getElementById("demo").innerHTML= 10 + 5
+}
